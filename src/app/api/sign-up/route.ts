77cd7ef3d5edc7e1929fd0ea8d94822af0efdc07
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-14 11:25:38 +0600

    make a post req

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,3 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/User.model";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import bcrypt from "bcryptjs"
+
+export async function POST(requset:Request){
+     await dbConnect()
+}
