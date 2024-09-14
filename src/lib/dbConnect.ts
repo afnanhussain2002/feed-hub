87e63ca9b40f4060d960daac3743da0172c07923
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-14 08:58:22 +0600

    write code if data base already connected or not

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -6,8 +6,11 @@ type connectionObject ={
 
 const connection : connectionObject = {}
 
-async function dbConnect(){
-
+async function dbConnect(): Promise<void>{
+     if (connection.isConnected) {
+        console.log('Database already connected');
+        return;
+     }
 }
 
 export default dbConnect;
