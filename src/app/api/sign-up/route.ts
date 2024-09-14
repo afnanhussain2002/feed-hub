5c88d5fd4bdcd886be6ab03ac956022becfd3b61
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-14 11:23:51 +0600

    install and import bcrypt js

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,3 +1,4 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/User.model";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import bcrypt from "bcryptjs"
