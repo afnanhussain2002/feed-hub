cb32676a16ceea6d0a31538da9518428d48563df
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-14 11:39:11 +0600

    handle catch part using re useable code

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,6 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/User.model";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import bcrypt from "bcryptjs"
+import { jsonResponse } from "@/types/JsonResponse";
 
 export async function POST(requset:Request){
      await dbConnect()
@@ -9,6 +10,6 @@ export async function POST(requset:Request){
         
      } catch (error) {
         console.log('Error registering user', error);
-        
+        return jsonResponse(false, "Error registering user", 500)
      }
 }
