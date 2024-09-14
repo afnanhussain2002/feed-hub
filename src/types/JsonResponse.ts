f694fba191a29ceab0e5e9ebb6fa44cc92a04cfb
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-14 11:36:46 +0600

    write reuseable code

--- src/types/JsonResponse.ts
+++ src/types/JsonResponse.ts
@@ -1,7 +1,15 @@
-
-
-export interface JsonResponse{
-    success:boolean
-    message:string
-    status:number
-}
+export const jsonResponse = (
+  success: boolean,
+  message: string,
+  status: number
+) => {
+  return Response.json(
+    {
+      success,
+      message,
+    },
+    {
+      status,
+    }
+  );
+};
