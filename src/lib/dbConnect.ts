f6fe1c81037d4107e16a136a82036de3ef8f4e5a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-14 09:16:57 +0600

    fix some issue when db connect

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,15 +1,28 @@
+import mongoose from "mongoose";
 
 
-type connectionObject ={
-    isConnected:number
+type ConnectionObject = {
+    isConnected?:number
 }
 
-const connection : connectionObject = {}
+const connection : ConnectionObject = {}
 
 async function dbConnect(): Promise<void>{
      if (connection.isConnected) {
         console.log('Database already connected');
         return;
+     }
+     try {
+       const db = await mongoose.connect(process.env.MONGODB_URI || '')
+
+       console.log('main database', db)
+      connection.isConnected = db.connections[0].readyState
+
+ console.log('database connection', db.connections);
+ console.log('Database connection successfully');
+     } catch (error) {
+        console.log('database connection failed');
+        process.exit(1)
      }
 }
 
