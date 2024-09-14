269ccf624f752fe492c0d2e7f654a84d06e558b7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-14 12:10:47 +0600

    write full code and save user for new registretion

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -20,12 +20,28 @@ export async function POST(request:Request){
        }
 
        const existingUserByEmail = await UserModel.findOne({email})
+       const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
 
        if (existingUserByEmail) {
         true
        }else{
         const hashedPassword = await bcrypt.hash(password, 10)
-        
+
+        const expiryDate = new Date()
+        expiryDate.setHours(expiryDate.getHours() + 1)
+
+        const newUser = new UserModel({
+            username,
+            email,
+            password:hashedPassword,
+            verifyCode,
+            verifyCodeExpiry:expiryDate,
+            isVerified: false,
+            isAcceptMessage:true,
+            messages:[]
+          })
+          
+          await newUser.save()
        }
 
      } catch (error) {
